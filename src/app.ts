3315dcfea2f640c5e916eed20cb7429418a49b53
Cyro Dubeux <xorycx@gmail.com>
2022-10-26 13:26:31 -0300

    added graceful shutdown

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,16 @@
 import * as dotenv from 'dotenv';
 
-dotenv.config({ path: __dirname + '../../.env' });
+dotenv.config({ path: __dirname + '/../.env' });
 
 import Fastify, { FastifyInstance } from 'fastify';
 import cors from '@fastify/cors';
 import { postgres } from './config/database';
 import Routes from './routes';
+import FastifyGracefulShutdown from 'fastify-graceful-shutdown';
 
 const fastify: FastifyInstance = Fastify({ logger: true });
 
-fastify.server.requestTimeout = 20000;
+fastify.server.requestTimeout = Number(process.env.SERVER_TIMEOUT) || 20000;
 
 fastify.register(cors, {
   origin: ['*'],
@@ -22,12 +23,33 @@ fastify.register(cors, {
 
 fastify.register(Routes);
 
+fastify.register(FastifyGracefulShutdown);
+
+fastify.addHook('onClose', async () => {
+  await postgres.end();
+  fastify.log.info('postgres closed');
+  fastify.log.info('server closed');
+});
+
+fastify.after(() => {
+  fastify.gracefulShutdown(async (signal: string, next: (err?: Error | undefined) => void) => {
+    try {
+      fastify.log.warn(signal + ' received...');
+      fastify.log.info('closing postgres...');
+      next();
+    } catch (err: any) {
+      fastify.log.error(err);
+      next(err);
+    }
+  });
+});
+
 (async () => {
   try {
     await postgres.connect();
     fastify.log.info('postgres started successfully');
 
-    await fastify.listen({ port: Number(process.env.PORT) });
+    await fastify.listen({ port: Number(process.env.SERVER_PORT) });
     fastify.log.info('server started successfully');
   } catch (err) {
     fastify.log.error(err);
